a61f42086e9ad94b094297d25e3ff7f137d12440
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-22 12:57:48 -0500

    Completed Assignment

---
+++ notes.rb
@@ -0,0 +1,7 @@
+# Example 
+my_hash = {first: "I'm first!", second: "I'm second!"}
+my_hash[:third] = "I'm third!"
+
+puts my_hash
+# > {first: "I'm first!", second: "I'm second!", third: "I'm third!"} 
+
